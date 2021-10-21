ac307e127ffbe83eedc097c65c3bca5a0b45983b
iamalmiir <iamalmiir@gmail.com>
2021-10-21 19:20:57 -0400

    more testing

--- controllers/posts.contoller.js
+++ controllers/posts.contoller.js
@@ -1,7 +1,202 @@
+const User = require('../models/User')
+const Post = require('../models/Post')
+
+// @route   POST api/users
+// @desc    Create post
+// @access  Private
+const createPosts = async (req, reply) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    })
+
+    const post = await newPost.save()
+
+    reply.send(post)
+  } catch (err) {
+    console.error(err.message)
+    replay.code(500).send('Server Error')
+  }
+}
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
 const getPosts = async (req, reply) => {
-  reply.send('Posts route')
+  try {
+    const posts = await Post.find().sort({ date: -1 })
+    reply.send(posts)
+  } catch (err) {
+    console.error(err.message)
+    reply.code(500).send('Server Error')
+  }
+}
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+const getPostById = async (req, reply) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+
+    res.status(500).send('Server Error')
+  }
+}
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+const deletePost = async (req, reply) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    await post.remove()
+
+    res.json({ msg: 'Post removed' })
+  } catch (err) {
+    console.error(err.message)
+
+    res.status(500).send('Server Error')
+  }
+}
+
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+const likePost = async (req, reply) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check if the post has already been liked
+    if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+      return res.status(400).json({ msg: 'Post already liked' })
+    }
+
+    post.likes.unshift({ user: req.user.id })
+
+    await post.save()
+
+    return res.json(post.likes)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+}
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+const unlikePost = async (req, reply) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check if the post has not yet been liked
+    if (!post.likes.some((like) => like.user.toString() === req.user.id)) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' })
+    }
+
+    // remove the like
+    post.likes = post.likes.filter(
+      ({ user }) => user.toString() !== req.user.id
+    )
+
+    await post.save()
+
+    return res.json(post.likes)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+}
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   Private
+const commentOnPost = async (req, reply) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    const post = await Post.findById(req.params.id)
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    }
+
+    post.comments.unshift(newComment)
+
+    await post.save()
+
+    res.json(post.comments)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+}
+
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+const deleteComment = async (req, reply) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    )
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' })
+    }
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    )
+
+    await post.save()
+
+    return res.json(post.comments)
+  } catch (err) {
+    console.error(err.message)
+    return res.status(500).send('Server Error')
+  }
 }
 
 module.exports = {
+  createPosts,
   getPosts,
+  getPostById,
+  deletePost,
+  likePost,
+  unlikePost,
+  commentOnPost,
+  deleteComment,
 }
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,16 +1,44 @@
 const auth = require('../../middleware/auth')
-const { getPosts } = require('../../controllers/posts.contoller')
+const {
+  getPosts,
+  getPostById,
+  deletePost,
+} = require('../../controllers/posts.contoller')
+const createPostsOpts = require('../../lib/posts.options')
 
 const getPostsOpts = {
   handler: getPosts,
 }
 
-const postsRoutes = (fastify, options, done) => {
-  // @route   POST api/users
-  // @desc    Register a user
-  // @access  Public
+const getPostByIdOpts = {
+  handler: getPostById,
+}
 
+const deletePosOpts = {
+  handler: deletePost,
+}
+
+const postsRoutes = (fastify, options, done) => {
+  fastify.addHook('preHandler', auth)
+  // @route   POST api/users
+  // @desc    Create post
+  // @access  Private
+  fastify.post('/api/posts', createPostsOpts)
+
+  // @route    GET api/posts
+  // @desc     Get all posts
+  // @access   Private
   fastify.get('/api/posts', getPostsOpts)
+
+  // @route    GET api/posts/:id
+  // @desc     Get post by ID
+  // @access   Private
+  fastify.get('/api/posts/:id', getPostByIdOpts)
+
+  // @route    DELETE api/posts/:id
+  // @desc     Delete a post
+  // @access   Private
+  fastify.delete('/api/posts/:id', deletePosOpts)
 
   done()
 }
