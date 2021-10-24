f55de240b313916522f75695e33c6e171026e2da
iamalmiir <iamalmiir@gmail.com>
2021-10-24 05:49:14 -0400

    added frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Client</h1>
+    </main>
+  )
+}
+
+export default App
